3562ee7a2c3e0fa16bf65caa66e3ff801fc9c811
pankaj <animineer.creation@gmail.com>
2025-08-25 17:52:57 +0530

    router

--- frontend/src/component/footer/Footer_1.jsx
+++ frontend/src/component/footer/Footer_1.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import style from "./Footer.module.css"
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 export const Footer_1 = () => {
   return (
     <div className={style.f1_main}>
         {/* <h3><Link to="/SHOP ALL">SHOP ALL</Link></h3> */}
-        <h3><Link to="/PHONES">PHONES</Link></h3>
-        <h3><Link to="/AUDIO">AUDIO</Link></h3>
-        <h3><Link to="/WATCHES">WATCHES</Link></h3>
-        <h3><Link to="/ACCESSORIES">ACCESSORIES</Link></h3>
-        <h3><Link to="/CMF">CMF</Link></h3>
+        <h3><Link to="/phones">PHONES</Link></h3>
+        <h3><Link to="/audio">AUDIO</Link></h3>
+        <h3><Link to="/watches">WATCHES</Link></h3>
+        <h3><Link to="/accessories">ACCESSORIES</Link></h3>
+        {/* <h3><Link to="/CMF">CMF</Link></h3> */}
+        <Outlet/>
     </div>
   )
 }
