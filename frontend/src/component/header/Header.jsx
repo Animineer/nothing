35c25470e008c3011b9ea1ce6f96f2ff0710bf74
pankaj <animineer.creation@gmail.com>
2025-08-26 15:02:05 +0530

    hd image updated

--- frontend/src/component/header/Header.jsx
+++ frontend/src/component/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import style from "./Header.module.css"
 import menu from "../../assets/icons/menu.svg"
 import cart from "../../assets/icons/cart.svg"
@@ -6,20 +6,23 @@ import logo from "../../assets/logo.png"
 import { Link } from 'react-router-dom'
 
 
+
 const Header = () => {
+  
+
 
   return (
     <>
-      
-        <div className={style.main}>
-          <img className={style.menu}src={menu} alt="" />
+       <div className={style.empty}>
+        <div  className={style.main}>
+          <img onClick={menubar}   className={style.menu}src={menu} alt="" />
           <div ><Link to="/"><img className={style.logo} src={logo} alt="" /></Link></div>
           <img className={style.cart} src={cart} alt="" />
         </div>
         <div className={style.menu_bar}>
 
         </div>
-      
+      </div>
     </>
   )
 }
